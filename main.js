060cab39193f2ff0cbbcec9d73a682f2cdd84786
William11Cassidy <121349385+William11Cassidy@users.noreply.github.com>
2023-04-24 15:38:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,97 @@
+
+canvas = new fabric.Canvas("myCanvas");
+ball_y = 10;
+ball_x = 10;
+hole_x = 800
+hole_y = 400
+block_image_width = 5;
+block_image_height = 5;
+
+function load_img() {
+	fabric.Image.fromURL("golf-h.png", function (img) {
+		holeobject = img;
+		holeobject.scaleToWidth(50);
+		holeobject.scaleToHeight(50);
+		holeobject.set({
+			top: hole_y,
+			left: hole_x
+		});
+		canvas.add(holeobject)
+	})
+	new_image();
+}
+
+function new_image() {
+	fabric.Image.fromURL("ball.png", function (img) {
+		ballobject = img;
+		ballobject.scaleToWidth(50);
+		ballobject.scaleToHeight(50);
+		ballobject.set({
+			top: ball_y,
+			left: ball_x
+		});
+		canvas.add(ballobject)
+	})
+
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e) {
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+
+	if ((ball_x == hole_x) && (ball_y == hole_y)) {
+		canvas.remove(ballobject)
+		document.getElementById("hd3").innerHTML = "you have won"
+		document.getElementById("myCanvas").style.borderColor = "red"
+	}
+	else {
+		if (keyPressed == '38') {
+			up();
+			console.log("up");
+		}
+		if (keyPressed == '40') {
+			down();
+			console.log("down");
+		}
+		if (keyPressed == '37') {
+			left();
+			console.log("left");
+		}
+		if (keyPressed == '39') {
+			right();
+			console.log("right");
+		}
+	}
+
+	function up() {
+		ball_y -= 10
+		canvas.remove(ballobject)
+		new_image()
+	}
+
+	function down() {
+		ball_y += 10
+		canvas.remove(ballobject)
+		new_image()
+	}
+
+	function left() {
+		if (ball_x > 5) {
+			ball_x -= 10
+			canvas.remove(ballobject)
+			new_image()
+		}
+	}
+
+	function right() {
+		if (ball_x <= 1050) {
+			ball_x += 10
+			canvas.remove(ballobject)
+			new_image()
+		}
+	}
+
+}
+
